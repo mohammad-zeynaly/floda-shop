9675aa60162bec9113ccc83c4618352009fe198c
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-06-01 16:56:36 +0330

    The ability to add a product to the shopping cart from the quick view modal. that upload to github-pages

--- script/modules/addProductToCart.js
+++ script/modules/addProductToCart.js
@@ -1,21 +1,41 @@
 import allProduct from "../../data/allData.js";
-import { userBasketCartArray, userBasketCartTemplate,} from "./userBasketCart.js";
-import totalPrice from "./totalPrice.js"
-import toastContainer from "./toastContainer.js"
+import {
+  userBasketCartArray,
+  userBasketCartTemplate,
+} from "./userBasketCart.js";
+import totalPrice from "./totalPrice.js";
+import toastContainer from "./toastContainer.js";
 
-export function addProductToCart(productId) {
+export function addProductToCart(productId, mainProductDetails) {
   if (userBasketCartArray) {
     let isProductBasket = userBasketCartArray.some(
       (cartProduct) => +productId === cartProduct.id
     );
-    if (!isProductBasket) {
-      let mainProduct = allProduct.find((product) => +productId === product.id);
 
-      userBasketCartArray.push(mainProduct);
+    let isMainProductDetails = userBasketCartArray.some(
+      (cartProduct) => +mainProductDetails.id === cartProduct.id
+    );
+
+    console.log("isMainProductDetails=> ",isMainProductDetails)
+
+    if (!isProductBasket && !isMainProductDetails) {
+      
+      if (!mainProductDetails) {
+        let mainProduct = allProduct.find(
+          (product) => +productId === product.id
+        );
+        console.log("محصول از فروشگاه اضافه شد ");
+        userBasketCartArray.push(mainProduct);
+        totalPrice(userBasketCartArray);
+
+        toastContainer("success", "محصول با موفقیت اضافه شد");
+      } else {
+        console.log("محصول از جزئیات اضافه شد ");
+        userBasketCartArray.push(mainProductDetails);
+        totalPrice(userBasketCartArray);
+        toastContainer("success", "محصول با موفقیت اضافه شد");
+      }
       userBasketCartTemplate(userBasketCartArray);
-      totalPrice(userBasketCartArray)
-
-      toastContainer("success","محصول با موفقیت اضافه شد")
     }
   }
 }
--- script/modules/detailsProduct.js
+++ script/modules/detailsProduct.js
@@ -1,23 +1,57 @@
-import allProduct from "../../data/allData.js"
-const productDetailsTitle = document.getElementById("product-details__title"),
-productDetailImage = document.getElementById("product-details__image"),
-productDetailsOriginalPrice = document.getElementById("product-details__original-price"),
-productDetailsDiscountPrice = document.getElementById("product-details__discount-price");
-let searchParameter = new URLSearchParams(location.search)
-let searchPageProducts = searchParameter.get("page")
+import allProduct from "../../data/allData.js";
+import { addProductToCart } from "./addProductToCart.js";
+const modalContainer = document.getElementById("quick_view"),
+  productDetailsTitle = document.getElementById("product-details__title"),
+  productDetailImage = document.getElementById("product-details__image"),
+  productDetailsOriginalPrice = document.getElementById(
+    "product-details__original-price"
+  ),
+  productDetailsDiscountPrice = document.getElementById(
+    "product-details__discount-price"
+  ),
+  addBasket = document.querySelector(".add__basket"),
+  modalQuickCloseBtn = document.querySelector(".modal-quick-close__btn");
+let searchParameter = new URLSearchParams(location.search);
+let searchPageProducts = searchParameter.get("page");
 
-export default function detailsProduct (productId) {
+export default function detailsProduct(productId) {
+  console.log("quick view product details=> ", productId);
 
-    console.log("quick view product details=> ",productId)
+  const mainProductDetails = allProduct.find(
+    (product) => +productId === product.id
+  );
 
-    const mainProductDetails =  allProduct.find(product => +productId === product.id)
+  if (mainProductDetails) {
+    productDetailsTitle.textContent = mainProductDetails.name;
+    productDetailImage.setAttribute(
+      "src",
+      searchPageProducts === "products"
+        ? "../" + mainProductDetails.img
+        : mainProductDetails.img
+    );
+    productDetailsOriginalPrice.textContent =
+      mainProductDetails.originalPrice + "تومان";
+    productDetailsDiscountPrice.textContent =
+      mainProductDetails.discountPrice + "تومان";
 
-    if(mainProductDetails){
 
-    productDetailsTitle.textContent = mainProductDetails.name
-    productDetailImage.setAttribute("src",searchPageProducts === "products" ? "../"+ mainProductDetails.img : mainProductDetails.img)
-    productDetailsOriginalPrice.textContent = mainProductDetails.originalPrice + "تومان"
-    productDetailsDiscountPrice.textContent = mainProductDetails.discountPrice + "تومان"
-
+    // click add shopping cart in product details
+    if (document.querySelector(".modal-shadow")) {
+      document.querySelector(".modal-shadow").className ="modal-backdrop show";
     }
+
+    addBasket.addEventListener("click", () => {
+      modalContainer.style.display = "modal-backdrop show";
+      addProductToCart(null, mainProductDetails);
+      const modalBackdrop = document.querySelector(".modal-backdrop");
+      modalContainer.style.display = "none";
+      document.body.style.cssText = "overflow: auto; padding-right: 0;";
+      modalBackdrop.className = "modal-shadow";
+    });
+
+  }
 }
+modalQuickCloseBtn.addEventListener(
+  "click",
+  () => (modalContainer.style.display = "none")
+);
