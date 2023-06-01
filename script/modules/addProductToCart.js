b95d0fade0570525ebb5d7fb55416d0a71a9bae3
Mohammad Zeynali <mhmdzynaly977@gmail.com>
2023-06-01 17:18:43 +0330

    fixed the bug of not adding products

--- script/modules/addProductToCart.js
+++ script/modules/addProductToCart.js
@@ -7,35 +7,40 @@ import totalPrice from "./totalPrice.js";
 import toastContainer from "./toastContainer.js";
 
 export function addProductToCart(productId, mainProductDetails) {
+
+
   if (userBasketCartArray) {
     let isProductBasket = userBasketCartArray.some(
       (cartProduct) => +productId === cartProduct.id
     );
+      console.log("mainProductDetails=> ",mainProductDetails)
+    if (mainProductDetails) {
+      console.log(mainProductDetails);
 
-    let isMainProductDetails = userBasketCartArray.some(
-      (cartProduct) => +mainProductDetails.id === cartProduct.id
-    );
+      let isMainProductDetails = userBasketCartArray.some(
+        (cartProduct) => +mainProductDetails.id === cartProduct.id
+      );
 
-    console.log("isMainProductDetails=> ",isMainProductDetails)
-
-    if (!isProductBasket && !isMainProductDetails) {
+      if (!isMainProductDetails) {
+        console.log("محصول از جزئیات اضافه شد ");
+        userBasketCartArray.push(mainProductDetails);
+        totalPrice(userBasketCartArray);
+        toastContainer("success", "محصول با موفقیت اضافه شد");
+        userBasketCartTemplate(userBasketCartArray);
+      }
       
-      if (!mainProductDetails) {
+    } else {
+      if (!isProductBasket) {
         let mainProduct = allProduct.find(
           (product) => +productId === product.id
         );
         console.log("محصول از فروشگاه اضافه شد ");
         userBasketCartArray.push(mainProduct);
         totalPrice(userBasketCartArray);
-
         toastContainer("success", "محصول با موفقیت اضافه شد");
-      } else {
-        console.log("محصول از جزئیات اضافه شد ");
-        userBasketCartArray.push(mainProductDetails);
-        totalPrice(userBasketCartArray);
-        toastContainer("success", "محصول با موفقیت اضافه شد");
+        userBasketCartTemplate(userBasketCartArray);
       }
-      userBasketCartTemplate(userBasketCartArray);
     }
+    
   }
 }
